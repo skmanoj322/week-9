abc3523c6f5351c164a4af6f948810ad1456caef
skmanoj322 <86718126+skmanoj322@users.noreply.github.com>
2023-08-09 11:51:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,14 +8,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.get("/",(req,res)=>res.json({msg:"hello word"}));
+// app.get("/",(req,res)=>res.json({msg:"hello word"}));
 
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
 
-// Connect to MongoDB
-// DONT MISUSE THIS THANKYOU!!
-mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+
 
 app.listen(3000, () => console.log('Server running on port 3000'));
