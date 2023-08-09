607f0a074cfa37c76e3082c2ffae9c00dba4ac69
skmanoj322 <sk.manoj322@gmail.com>
2023-08-09 13:01:33 +0530

    "deploy file changes "

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
 
-cd Week-5-with-axios-and-backend
- git pull origin master
+cd week-9
+ git pull origin2 master
  cd server
  pm2 kill
  pm2 start index.js
